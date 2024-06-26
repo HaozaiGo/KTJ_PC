526007a665f620dd7a31b4600cd3d41cc926411d
JiaHao <1029172530@qq.com>
2024-06-26 16:22:48 +0800

    build-try1.3

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import commonjs from 'vite-plugin-commonjs';
+import commonjs from 'vite-plugin-commonjs'
 export default defineConfig({
   plugins: [vue(), vueJsx(),commonjs()],
   resolve: {
