d3d5bbdbd3a638dede2e14a7f5dd3b1c27d9ae26
JiaHao <1029172530@qq.com>
2025-01-14 16:48:08 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import commonjs from 'vite-plugin-commonjs';
 export default defineConfig({
-  base:"/dev/dest/",
+  base:"/signup/",
   plugins: [vue(), vueJsx(), commonjs()],
   resolve: {
     alias: {
